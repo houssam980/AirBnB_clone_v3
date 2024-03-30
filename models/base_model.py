6fdbb34e3e75be04bd176fd928650bf6da8699b1
houssam980 <houssam.ghmari1243@gmail.com>
2024-03-30 15:26:58 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,66 @@
 #!/usr/bin/python3
-"""
-Defines Base module
-"""
-from uuid import uuid4
+'''
+  cass module
+'''
+from os import getenv
+import uuid
 from datetime import datetime
-from models import storage
+import models
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """
-    Define the BaseModel class
-    """
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """
-        Initialize  BaseModel instance.
-
-        Parameters
-        args : arguments.
-        kwargs : A key-worded arguments.
-        """
-        if (kwargs == {}):
-            self.id = str(uuid4())
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            for k, v in kwargs.items():
-                if (k == "__class__"):
-                    continue
-                setattr(self, k, datetime.fromisoformat(v)
-                        if ("_at" in k) else v)
+            if kwargs.get("created_at"):
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("created_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
+            if not self.id:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
-        """
-        Return a string
-        """
-        return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def __repr__(self):
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
-        """
-        Updates attr
-        """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """
-        Return dictionary of attributes
-        """
-        dict_obj = {}
+        cp_dct = dict(self.__dict__)
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        if hasattr(self, "_sa_instance_state"):
+            del cp_dct["_sa_instance_state"]
+        return (cp_dct)
 
-        for k, v in self.__dict__.items():
-            dict_obj[k] = v.isoformat() if ("_at" in k) else v
-        dict_obj["__class__"] = type(self).__name__
-        return (dict_obj)
-
+    def delete(self):
+        models.storage.delete(self)
--- models/user.py
+++ models/user.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""user module"""
-
-import models
-import sqlalchemy
+'''
+    user modele
+'''
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from os import getenv
-
-
 class User(BaseModel, Base):
-    """User creation"""
-
-    
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """user initializing"""
-        super().__init__(*args, **kwargs)
